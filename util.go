aec6bd4027b639c95355edf75844279b984a4181
jonc <jonpchin@gmail.com>
2016-10-09 23:50:40 -0400

    Completed command line parsing, need to test it

--- util.go
+++ util.go
@@ -2,8 +2,11 @@ package gostuff
 
 import (
 	"fmt"
-	"github.com/mholt/archiver"
+	"io/ioutil"
 	"os"
+	"strings"
+
+	"github.com/mholt/archiver"
 )
 
 // Returns true if a given file or directory exist in the path
@@ -33,6 +36,32 @@ func unzip(source string, destination string) bool {
 	err := archiver.Unzip(source, destination)
 	if err != nil {
 		fmt.Println("util.go unzip There was an error in archiving the file", err)
+		return false
+	}
+	return true
+}
+
+// Replaces target string in file with desired string in the file path
+// Returns false if there was an error in the operation
+func ReplaceString(target, desired, source string) bool {
+
+	input, err := ioutil.ReadFile(source)
+	if err != nil {
+		fmt.Println("Failed to replaceString 1 util.go", err)
+		return false
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, target) {
+			lines[i] = strings.Replace(lines[i], target, desired, -1)
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(source, []byte(output), 0644)
+	if err != nil {
+		fmt.Println("Failed to replaceString 2 util.go", err)
 		return false
 	}
 	return true
