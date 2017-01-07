3fe82018723c12ab6ed7487649722673d80348ff
jonc <jonpchin@gmail.com>
2017-01-07 01:31:50 -0500

    trying to resolve travis build

--- _travis/database_test.go
+++ _travis/database_test.go
@@ -12,11 +12,12 @@ var db *sql.DB
 // Travis CI default MySQL username and pass is public information
 func TestDbConnect(t *testing.T) {
 	// make sure MySQL connection is alive before proceeding
-	if gostuff.CheckDBConnection() == false {
+	if gostuff.CheckDBConnection("data/dbtravis.txt") == false {
 		t.Fatal("Failed to connect to MySQL in Travis CI")
 	}
 	dbString, _ := gostuff.ReadFile("data/dbtravis.txt")
-	db, err = sql.Open("mysql", dbString)
+	db, err := sql.Open("mysql", dbString)
+	defer db.Close()
 	//	db.SetMaxIdleConns(20)
 	if err != nil {
 		t.Fatal("Can't open MySQL")
@@ -26,4 +27,5 @@ func TestDbConnect(t *testing.T) {
 	if db.Ping() != nil {
 		t.Fatal("Can't ping MySQL")
 	}
+
 }
