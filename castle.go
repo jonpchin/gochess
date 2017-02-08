25e6d7075d53236e4fd4c6fa10828e9c4d7ef69e
jonc <jonpchin@gmail.com>
2017-02-08 01:51:22 -0500

    adding newsfeed

--- castle.go
+++ castle.go
@@ -3,28 +3,32 @@ package gostuff
 //can't castle in check or through check, can't castle when king has already moved
 func (table *Table) canWhiteCastleKing() bool {
 
-	if table.wkMoved == false && table.wkrMoved == false && table.canBlackKillSquare(7, 4) == false && table.canBlackKillSquare(7, 5) == false && table.canBlackKillSquare(7, 6) == false {
+	if table.wkMoved == false && table.wkrMoved == false && table.canBlackKillSquare(7, 4) == false &&
+		table.canBlackKillSquare(7, 5) == false && table.canBlackKillSquare(7, 6) == false {
 		return true
 	}
 	return false
 }
 
 func (table *Table) canBlackCastleKing() bool {
-	if table.bkMoved == false && table.bkrMoved == false && table.canWhiteKillSquare(0, 4) == false && table.canWhiteKillSquare(0, 5) == false && table.canWhiteKillSquare(0, 6) == false {
+	if table.bkMoved == false && table.bkrMoved == false && table.canWhiteKillSquare(0, 4) == false &&
+		table.canWhiteKillSquare(0, 5) == false && table.canWhiteKillSquare(0, 6) == false {
 		return true
 	}
 	return false
 }
 
 func (table *Table) canWhiteCastleQueen() bool {
-	if table.wkMoved == false && table.wqrMoved == false && table.canBlackKillSquare(7, 3) == false && table.canBlackKillSquare(7, 2) == false {
+	if table.wkMoved == false && table.wqrMoved == false && table.canBlackKillSquare(7, 3) == false &&
+		table.canBlackKillSquare(7, 2) == false {
 		return true
 	}
 	return false
 }
 
 func (table *Table) canBlackCastleQueen() bool {
-	if table.bkMoved == false && table.bqrMoved == false && table.canWhiteKillSquare(0, 3) == false && table.canWhiteKillSquare(0, 2) == false {
+	if table.bkMoved == false && table.bqrMoved == false && table.canWhiteKillSquare(0, 3) == false &&
+		table.canWhiteKillSquare(0, 2) == false {
 		return true
 	}
 	return false
