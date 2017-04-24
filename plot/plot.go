62e24bed0c7759c952d8d361acc146c6337f5570
jonc <jonpchin@gmail.com>
2017-04-24 00:32:05 -0400

    Rating plot back end completed

--- plot/plot.go
+++ plot/plot.go
@@ -2,66 +2,116 @@ package plot
 
 import (
 	"bufio"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/jonpchin/gochess/gostuff"
 	chart "github.com/wcharczuk/go-chart"
 )
 
-func DrawChart(res http.ResponseWriter, req *http.Request) {
+func DrawChart(w http.ResponseWriter, r *http.Request) {
 
+	valid := gostuff.ValidateCredentials(w, r)
+
+	if valid == false {
+		return
+	}
+
+	username, _ := r.Cookie("username")
 	log := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
 
+	ratingList := []string{"bullet", "blitz", "standard", "correspondence"}
+
+	// Index of these double arrays follows same order as ratingList
+	var allRatingDates [][]time.Time
+	var allRatings [][]float64
+
+	for _, value := range ratingList {
+
+		ratingHistory, pass, err := gostuff.GetRatingHistory(username.Value, value)
+		if pass == false {
+			log.Println(err)
+			return
+		}
+
+		var ratingMemory []gostuff.RatingDate
+		var ratingDates []time.Time
+		var ratings []float64
+
+		if ratingHistory != "" {
+			if err := json.Unmarshal([]byte(ratingHistory), &ratingMemory); err != nil {
+				log.Println("Just receieved a message I couldn't decode:", ratingHistory, err)
+				return
+			}
+
+			timeFormat := "20060102150405"
+
+			for _, value := range ratingMemory {
+				dateTime, err := time.Parse(timeFormat, value.DateTime)
+				if err != nil {
+					log.Println(err)
+				}
+				ratingDates = append(ratingDates, dateTime)
+				ratings = append(ratings, value.Rating)
+			}
+
+			allRatingDates = append(allRatingDates, ratingDates)
+			allRatings = append(allRatings, ratings)
+
+		} else {
+			// Then the player has no rating in this gametype
+			allRatingDates = append(allRatingDates, nil)
+			allRatings = append(allRatings, nil)
+		}
+	}
+
 	graph := chart.Chart{
+		Title: "Rating History",
 		XAxis: chart.XAxis{
-			Style: chart.Style{
-				Show: true,
-			},
+			Name:      "Time",
+			NameStyle: chart.StyleShow(),
+			Style:     chart.StyleShow(),
+		},
+		YAxis: chart.YAxis{
+			Name:      "Rating",
+			NameStyle: chart.StyleShow(),
+			Style:     chart.StyleShow(),
 		},
 		Series: []chart.Series{
 			chart.TimeSeries{
-				XValues: []time.Time{
-					time.Now().AddDate(0, 0, -10),
-					time.Now().AddDate(0, 0, -9),
-					time.Now().AddDate(0, 0, -8),
-					time.Now().AddDate(0, 0, -7),
-					time.Now().AddDate(0, 0, -6),
-					time.Now().AddDate(0, 0, -5),
-					time.Now().AddDate(0, 0, -4),
-					time.Now().AddDate(0, 0, -3),
-					time.Now().AddDate(0, 0, -2),
-					time.Now().AddDate(0, 0, -1),
-					time.Now(),
-				},
-				YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0},
+				Name:    "Bullet",
+				XValues: allRatingDates[0],
+				YValues: allRatings[0],
 			},
 			chart.TimeSeries{
-				XValues: []time.Time{
-					time.Now().AddDate(0, 0, -9),
-					time.Now().AddDate(0, 0, -8),
-					time.Now().AddDate(0, 0, -7),
-					time.Now().AddDate(0, 0, -6),
-					time.Now().AddDate(0, 0, -5),
-					time.Now().AddDate(0, 0, -4),
-					time.Now().AddDate(0, 0, -3),
-					time.Now().AddDate(0, 0, -2),
-					time.Now().AddDate(0, 0, -1),
-					time.Now().AddDate(0, 0, 0),
-					time.Now(),
-				},
-				YValues: []float64{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0},
+				Name:    "Blitz",
+				XValues: allRatingDates[1],
+				YValues: allRatings[1],
+			},
+			chart.TimeSeries{
+				Name:    "Standard",
+				XValues: allRatingDates[2],
+				YValues: allRatings[2],
+			},
+			chart.TimeSeries{
+				Name:    "Correspondence",
+				XValues: allRatingDates[3],
+				YValues: allRatings[3],
 			},
 		},
 	}
-	//res.Header().Set("Content-Type", "image/png")
-
-	output, err := os.Create("img/plots/output.png")
+	//w.Header().Set("Content-Type", "image/png")
+	graph.Elements = []chart.Renderable{
+		chart.Legend(&graph),
+	}
+	output, err := os.Create("img/plots/" + username.Value + ".png")
 	if err != nil {
 		log.Println(err)
 	}
 	defer output.Close()
-	w := bufio.NewWriter(output)
-	graph.Render(chart.PNG, w)
+	fileWriter := bufio.NewWriter(output)
+	graph.Render(chart.PNG, fileWriter)
 }
