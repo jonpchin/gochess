1bcb3b59f8ea9fd225f4e5df4287530d0e40a9d3
jonc <jonpchin@gmail.com>
2017-05-12 00:33:08 -0400

    Worked on equipment layout

--- mud/equipment.go
+++ mud/equipment.go
@@ -1,18 +1,73 @@
 package mud
 
 type Equipment struct {
-	Weapon Weapon
+	Weapon   Weapon
+	Sidearm  Sidearm
+	Shield   Shield
+	Helmet   Helmet
+	Torso    Torso
+	Belt     Belt
+	Arms     Arms
+	Legs     Legs
+	Shoes    Shoes
+	Ring     Ring
+	Floating Floating
 }
 
 type Weapon struct {
 	Object
-	Strength string
+	Strength int
 	Effect   string // Not all weapons have special effects
+}
+
+type Sidearm struct {
+	Object
+	Strength int
+	Effect   string // Not all weapons have special effects
+}
+
+type Helmet struct {
+	Armor
+}
+
+type Shield struct {
+	Armor
+}
+
+type Torso struct {
+	Armor
+}
+
+type Belt struct {
+	Armor
+}
+
+type Arms struct {
+	Armor
+}
+
+type Legs struct {
+	Armor
+}
+
+type Shoes struct {
+	Armor
+}
+
+type Ring struct {
+	Armor
+}
+
+type Floating struct {
+	Armor
 }
 
 type Armor struct {
 	Object
-	Effect string
+	SharpProtection int // Sharp object physical defense such as spears, axe, daggers
+	BluntProtection int // Blunt objct physical defense such as clubs, mace, brass knuckles
+	Resistance      int // Magical defense
+	Effect          string
 }
 
 type Dagger struct {
--- mud/player.go
+++ mud/player.go
@@ -3,4 +3,21 @@ package mud
 type Player struct {
 	Name      string
 	Inventory []interface{} // What the player is carrying
+	Equipment Equipment
+	Stats     PlayerStats
+	Status    []string // List of afflictions or buffs affecting player
+	Bleed     int      // Amount of health the player will lose every tick
+	Level     int
+}
+
+type PlayerStats struct {
+	Name         string
+	Health       int
+	Mana         int
+	Energy       int
+	Strength     int
+	Speed        int
+	Dexterity    int
+	Intelligence int
+	Wisdom       int
 }
