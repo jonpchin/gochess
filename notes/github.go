7c779ec23b5d609b1c097eeaafdcf1342bf324ca
jonc <jonpchin@gmail.com>
2017-09-30 23:57:35 -0400

    release notes prototype complete

--- notes/github.go
+++ notes/github.go
@@ -2,20 +2,24 @@ package notes
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/google/go-github/github"
 )
 
-func GetAllClosedCommits() {
-
+func writeCommitsToJSONfile() {
 	client := github.NewClient(nil)
 
 	context := context.Background()
-	opts := &github.SearchOptions{Sort: "created", Order: "asc"}
+	var listOptions github.ListOptions
+	listOptions.Page = 1
 
-	page := 1
-	query := "repo:jonpchin/gochess"
+	opts := &github.SearchOptions{Sort: "created", Order: "asc", ListOptions: listOptions}
+
+	query := "repo:jonpchin/gochess state:closed"
 
 	results, _, err := client.Search.Issues(context, query, opts)
 	if err != nil {
@@ -24,15 +28,26 @@ func GetAllClosedCommits() {
 	}
 	fmt.Println("Total issues are ", *results.Total)
 
+	f, err := os.OpenFile("release_notes.json", os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Can't open release notes file", err)
+	}
+
+	defer f.Close()
+
 	for _, value := range results.Issues {
-		fmt.Println(value)
+		releaseNotes, _ := json.MarshalIndent(value, "", "    ")
+		_, err = f.Write([]byte(releaseNotes))
+		if err != nil {
+			fmt.Println("Couldn't write to release notes file", err)
+		}
 	}
 
 	*results.Total -= 30
 	for *results.Total > 0 {
 		*results.Total -= 30
-		page += 1
-		query := "repo:jonpchin/gochess"
+		listOptions.Page += 1
+		opts = &github.SearchOptions{Sort: "created", Order: "asc", ListOptions: listOptions}
 
 		results, _, err := client.Search.Issues(context, query, opts)
 		if err != nil {
@@ -41,7 +56,63 @@ func GetAllClosedCommits() {
 		}
 
 		for _, value := range results.Issues {
-			fmt.Println(value)
+			releaseNotes, _ := json.MarshalIndent(value, "", "    ")
+			_, err = f.Write([]byte(releaseNotes))
+			if err != nil {
+				fmt.Println("Couldn't write to release notes file", err)
+			}
+		}
+	}
+}
+
+func GetAllClosedCommits() {
+
+	client := github.NewClient(nil)
+
+	context := context.Background()
+	var listOptions github.ListOptions
+	listOptions.Page = 1
+	opts := &github.SearchOptions{Sort: "created", Order: "asc", ListOptions: listOptions}
+
+	query := "repo:jonpchin/gochess state:closed"
+
+	results, _, err := client.Search.Issues(context, query, opts)
+	if err != nil {
+		fmt.Println("Unable to search issues", err)
+		return
+	}
+
+	fmt.Println("Total issues are ", *results.Total)
+	release_notes := "release_notes.txt"
+	os.Remove(release_notes)
+
+	notes, err := os.OpenFile(release_notes, os.O_CREATE|os.O_APPEND, 0644)
+	defer notes.Close()
+	if err != nil {
+		fmt.Println("Can't open release notes text file", err)
+	}
+
+	t := time.Now()
+	notes.WriteString(t.Format("2006-01-02") + "\n")
+
+	for _, value := range results.Issues {
+		notes.WriteString(*value.Title + "\n")
+	}
+
+	*results.Total -= 30
+	for *results.Total > 0 {
+		*results.Total -= 30
+		listOptions.Page += 1
+		opts = &github.SearchOptions{Sort: "created", Order: "asc", ListOptions: listOptions}
+
+		results, _, err := client.Search.Issues(context, query, opts)
+		if err != nil {
+			fmt.Println("Unable to search issues", err)
+			return
+		}
+
+		for _, value := range results.Issues {
+			notes.WriteString(*value.Title + "\n")
 		}
 	}
 }
